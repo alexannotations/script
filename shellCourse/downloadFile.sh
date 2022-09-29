16e06504330c0b39bf2881cd92861a6eaf5cab82
alexayca <90019860+alexayca@users.noreply.github.com>
2022-09-28 22:52:25 -0500

    descargar info de internet

---
+++ shellCourse/downloadFile.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+# Descarga de información desde internet utilizando el comando wget
+
+# Para archivos pequeños ~> wget
+# Para comunicarse con un servicio (por ejemplo, una API Restful) ~> curl
+# Para archivos de tamaño considerable ~> aria2
+#  lynx (Navegador web por consola)
+
+
+echo "Descargar información de internet"
+wget https://es-mx.wordpress.org/latest-es_MX.tar.gz
