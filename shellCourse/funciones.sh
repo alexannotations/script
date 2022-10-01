6e606d352f328b7b51a85e9978871176370401dd
alexayca <90019860+alexayca@users.noreply.github.com>
2022-10-01 00:43:54 -0500

    completando funciones

--- shellCourse/funciones.sh
+++ shellCourse/funciones.sh
@@ -7,26 +7,95 @@ opcion=0
 
 # Función para instalar
 instalar () {
-    echo "Instalar ..."
+    echo -e "\n Verificar instalación postgres ...."
+    verifyInstall=$(which psql) # se almacena la verificacion de instalacion, 
+    # llamando a un comando dentro de un script
+    # si es igual a cero si se encontro la instruccion, si es otro valor falla
+    # $?, el código de salida del pipe más reciente 
+    # encadenados varios comandos mediante el carácter pipe que se escribe como |
+    if [ $? -eq 0 ]; then
+        echo -e "\n Postgres ya se encuentra instalado en el equipo"
+    else
+        read -s -p "Ingresar contraseña de sudo:" password
+        read -s -p "Ingresar contraseña a utilizar en postgres:" passwordPostgres
+        echo "$password" | sudo -S apt update
+        echo "$password" | sudo -S apt-get -y install postgresql postgresql-contrib
+        sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '{$PASSWORDpOSTGRES}';"
+        echo "$password" | sudo -S systemctl enable postgresql.service
+        echo "$password" | sudo -S systemctl start postgresql.service
+    fi    
+    read -n 1 -s -r -p "PRESIONE [ENTER] para continuar..."
 }
+
 
 # Función para desinstalar 
 desinstalar () {
     echo "Desinstalar ..."
+    read -s -p "Ingresar contraseña de sudo:" password
+    echo -e "\n"
+    echo "$password" | sudo -S systemctl stop postgresql.service
+    echo "$password" | sudo -S apt-get -y --purge remove postgresql\*
+    echo "$password" | sudo -S rm -r /etc/postgresql
+    echo "$password" | sudo -S rm -r /etc/postgresql-common
+    echo "$password" | sudo -S rm -r /var/lib/postgresql
+    echo "$password" | sudo -S userdel -r postgres
+    echo "$password" | sudo -S groupdel postgresql
+    read -n 1 -s -r -p "PRESIONE [ENTER] para continuar..."
 }
+
 
 # Función para respaldar
 sacar_respaldo () {
     echo "Hacer respaldo..."
     # mostramos el parametro $1 recibido $directorioBackup
     echo "Directorio backup: $1"
+    # #
+    echo "Listar las bases de datos"
+    sudo -u postgres psql -c "\l"
+    read -p "Elegir la base de datos a respaldar:" bddRespaldo
+    echo -e "\n"
+    if [ -d "$1" ]; then
+        echo "Establecer permisos directorio"
+        echo "$password" | sudo -S chmod 755 $1
+        echo "Realizando respaldo..."
+        sudo -u postgres pg_dump -Fc $bddRespaldo > "$1/bddRespaldo$fechaActual.bak"
+        echo "Respaldo realizado correctamente en la ubicación:$1/bddRespaldo$fechaActual.bak"
+    else
+        echo "El directorio $1 no existe"
+    fi
+    read -n 1 -s -r -p "PRESIONE [ENTER] para continuar..."
 }
+
 
 # Función para restaurar un respaldo
 restaurar_respaldo () {
     echo "Restaurar respaldo..."
     echo "Directorio respaldo: $1"
+
+    echo "Listar respaldos"
+    ls -1 $1/*.bak
+    read -p "Elegir el respaldo a restaurar:" respaldoRestaurar
+    echo -e "\n"
+    read -p "Ingrese el nombre de la base de datos destino:" bddDestino
+    #Verificar si la bdd existe
+    verifyBdd=$(sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -wq $bddDestino)
+    if [ $? -eq 0 ]; then
+        echo "Restaurando en la bdd destino: $bddDestino"
+    else
+        sudo -u postgres psql -c "create database $bddDestino"
+    fi
+
+    if [ -f "$1/$respaldoRestaurar" ]; then
+        echo "Restaurando respaldo..."
+        sudo -u postgres pg_restore -Fc -d $bddDestino "$1/$respaldoRestaurar"
+        echo "Listar la base de datos"
+        sudo -u postgres psql -c "\l"
+    else
+        echo "El respaldo $respaldoRestaurar no existe"
+    fi    
+    read -n 1 -s -r -p "PRESIONE [ENTER] para continuar..."    
 }
+
 
 
 while :
